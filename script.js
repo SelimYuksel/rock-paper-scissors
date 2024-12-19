3cc5acdd0f3399e661bab046d438389a0f5139c0
SelimYuksel <yukselselim26@gmail.com>
2024-12-19 23:39:11 +0300

    create getHumanChoice function
    write algorithm for getHumanChoice

--- script.js
+++ script.js
@@ -2,15 +2,32 @@ const getComputerChoice = () => {
     const randomNumber = Math.floor(Math.random() * 3) + 1;
     /* This returns a random number between 1 and 3 so 1,2,3 */
     if(randomNumber === 1) {
-        return "Rock";
+        return "rock";
     } else if (randomNumber === 2) {
-        return "Paper";
+        return "paper";
     } else if(randomNumber === 3) {
-        return "Scissor";
+        return "scissors";
     }
 }
 
 // console.log(getComputerChoice());
 
+const getHumanChoice = () => {
+    const userInput = prompt("Rock, Paper, Scissors?");
 
+    const modifiedInput = userInput.toLowerCase();
+
+    if(modifiedInput === "") {
+        alert("Please enter something.");
+    } else if(modifiedInput !== "rock" && modifiedInput !== "paper" && modifiedInput !== "scissors") {
+        alert("Please enter a valid response.");
+    }
+    else {
+        return modifiedInput;
+    }
+    
+    
+}
+
+// console.log(getHumanChoice());
 
